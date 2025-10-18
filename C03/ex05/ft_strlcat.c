5866f446d3ca314399164f80e403b00123d1b8a1
Anna Melqonanyan <melqonanyanna@gmail.com>
2025-10-18 23:56:22 +0400

    ex05

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -2,11 +2,46 @@
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	
+	unsigned int dest_length;
+	unsigned int src_length;
+	unsigned int i;
+
+	dest_length = 0;
+	src_length = 0;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		dest_length++;
+		i++;
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		src_length++;
+		i++;
+	}
+
+	if (size <= dest_length)
+		return (dest_length + src_length);
+
+	i = 0;
+	while (src[i] != '\0' && (dest_length + i) < size - 1)
+	{
+		dest[dest_length + i] = src[i];
+		i++;
+	}
+	dest[dest_length + i] = '\0';
+	return (dest_length + src_length);  
 }
 
 int main()
 {
+	char src[] = "heyllo";
+	char dest[20] = "madame ";
+
+	int num = ft_strlcat(dest, src, 20);
 	
+	
+	printf("%s and %d", dest, num);
 	return (0);
 }
